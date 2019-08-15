2cbb852cc022bcb65227e9ee9f3b927835a103ea
GhostTalker <49254289+GhostTalker@users.noreply.github.com>
2019-08-16 01:29:48 +0200

    commandline options, -r (rgc) -p (pokemon) -d (pogodroid) -c (clear cache)

--- update_mad.sh
+++ update_mad.sh
@@ -1,44 +1,116 @@
 #!/system/bin/sh
 # update mad
+# version 2.0.1
+# created by GhostTalker
 #
-# install:
-# su
-# mount -o rw,remount /system
-# cp /<SCRIPTPATH>/update_mad.sh /system/bin/update_mad
-# chmod 555 /system/bin/<script_name>
-# mount -o ro,remount /system
-#
-# just run: /usr/bin/adb -s <DEVICEIP>:5555 shell su -c "update_mad"
 
-# note from krz
-# Before this goes into RC5 I will make every app (and cache clearing) be commandline options, -r (rgc) -p (pokemon) -d (pogodroid) -c (clear cache)
-# Please feel free to add this code before I do, if you like... Otherwise I'll get to it sometime soonish
+function stop_mad(){
+	echo "stopping MAD processes"
+	/system/bin/killall com.mad.pogodroid
+	/system/bin/am force-stop de.grennith.rgc.remotegpscontroller
+	echo ""
+}
 
-#delete old apk
-echo "Delete old APK"
-/system/bin/rm -f /sdcard/Download/RemoteGpsController.apk
-/system/bin/rm -f /sdcard/Download/PogoDroid.apk
-/system/bin/rm -f /sdcard/Download/pogo.apk
+function reboot_device(){
+	echo "Rebooting Device"
+	/system/bin/reboot
+}
 
-#download apk
-echo "Download APK"
-cd /sdcard/Download/
-/system/bin/curl -o RemoteGpsController.apk -k -s https://raw.githubusercontent.com/Map-A-Droid/MAD/master/APK/RemoteGpsController.apk
-/system/bin/curl -o pogodroid.apk -k -s https://www.maddev.de/apk/PogoDroid.apk
-/system/bin/curl -L -o pogo.apk -k -s "$(curl -k -s 'https://m.apkpure.com/pokemon-go/com.nianticlabs.pokemongo/download' | grep 'click here'|awk -F'"' '{print $12}')"
+function update_pogodroid(){
+	echo "updating PogoDroid..."
+	echo "Delete old APK PogoDroid"
+	/system/bin/rm -f /sdcard/Download/PogoDroid.apk
+	echo "Download APK PogoDroid"
+	cd /sdcard/Download/
+	/system/bin/curl -o pogodroid.apk -k -s https://www.maddev.de/apk/PogoDroid.apk
+	echo "Install APK PogoDroid"
+	/system/bin/pm install -r /sdcard/Download/PogoDroid.apk
+	echo ""
+}
 
-#stop mad
-echo "stopping MAD processes"
-/system/bin/killall com.mad.pogodroid
-/system/bin/am force-stop de.grennith.rgc.remotegpscontroller
+function update_rgc(){
+	echo "updating RGC..."
+	echo "Delete old APK RGC"
+	/system/bin/rm -f /sdcard/Download/RemoteGpsController.apk
+	echo "Download APK RGC"
+	cd /sdcard/Download/
+	/system/bin/curl -o RemoteGpsController.apk -k -s https://raw.githubusercontent.com/Map-A-Droid/MAD/master/APK/RemoteGpsController.apk
+	echo "Install APK RGC"
+	/system/bin/pm install -r /sdcard/Download/RemoteGpsController.apk
+	echo ""
+}
 
-#install apk
-echo "Install APK"
-/system/bin/pm install -r /sdcard/Download/RemoteGpsController.apk
-/system/bin/pm install -r /sdcard/Download/PogoDroid.apk
-/system/bin/pm install -r /sdcard/Download/pogo.apk
-/system/bin/pm clear com.nianticlabs.pokemongo
+function update_pokemon(){
+	echo "updating PokemonGo..."
+	echo "Delete old APK PokemonGo"
+	/system/bin/rm -f /sdcard/Download/pogo.apk
+	echo "Download APK PokemonGo"
+	cd /sdcard/Download/
+	/system/bin/curl -L -o pogo.apk -k -s "$(curl -k -s 'https://m.apkpure.com/pokemon-go/com.nianticlabs.pokemongo/download' | grep 'click here'|awk -F'"' '{print $12}')"
+	echo "Install APK PokemonGo"
+	/system/bin/pm install -r /sdcard/Download/pogo.apk
+	if [ $ClearCache == "True" ];
+		then
+			echo "clearing cache of app pokemongo"
+			/system/bin/pm clear com.nianticlabs.pokemongo;
+	fi
+	echo ""
+}
 
-#reboot device
-echo "Rebooting Device"
-/system/bin/reboot
+function print_help(){
+	echo "install:"
+	echo "su"
+	echo "mount -o rw,remount /system"
+	echo "cp /<SCRIPTPATH>/update_mad.sh /system/bin/update_mad"
+	echo "chmod 555 /system/bin/update_mad"
+	echo "mount -o ro,remount /system"
+	echo ""
+	echo "just run: /usr/bin/adb -s <DEVICEIP>:5555 shell su -c \"update_mad <options>\""
+	echo ""
+	echo "Options:"
+	echo "          -r   (Update RemoteGPSController)"
+	echo "          -p   (Update PokemonGO)"
+	echo "          -d   (Update PogoDroid)"
+	echo "          -a   (Update all)"
+	echo "          -c   (ClearCache of PokemonGo)"
+}
+
+UpdateRGC=False
+UpdatePoGo=False
+UpdatePogoDroid=False
+ClearCache=False
+
+for i in "$@"
+do
+	case  $i  in
+		-r)
+			UpdateRGC=True
+			;;
+		-p)
+			UpdatePoGo=True
+			;;
+		-d)
+			UpdatePogoDroid=True
+			;;
+		-a)
+			UpdateRGC=True
+			UpdatePoGo=True
+			UpdatePogoDroid=True
+			;;
+		-c)
+			ClearCache=True
+			;;
+		*)
+			print_help
+			exit
+			;;
+	esac
+done
+
+if [ $UpdateRGC == "True" ] || [ $UpdatePogoDroid == "True" ] || [ $UpdatePoGo == "True" ]; then stop_mad; fi
+if [ $UpdateRGC == "True" ]; then update_rgc; fi
+if [ $UpdatePogoDroid == "True" ]; then update_pogodroid; fi
+if [ $UpdatePoGo == "True" ]; then update_pokemon; fi
+if [ $UpdateRGC == "True" ] || [ $UpdatePogoDroid == "True" ] || [ $UpdatePoGo == "True" ]; then reboot_device; fi
+
+exit
