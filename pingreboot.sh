bbc1754f2fc0bc29dfb67598d0d95b5636b245b1
Heiko <49860648+sprintnudel@users.noreply.github.com>
2019-11-07 07:22:38 +0100

    pingreboot: make script customizable, add docs

--- pingreboot.sh
+++ pingreboot.sh
@@ -1,20 +1,38 @@
 #!/system/bin/sh
-#version 1.0
+#version 2.0
 
-[[ -f /sdcard/pingreboot ]] || exit
+CONF_FILE=/sdcard/pingreboot
+
+[ -f $CONF_FILE ] || exit
+
+log -t ProtoWifi "pingreboot: started"
+
+# These values can be overridden by putting them
+# in $CONF_FILE with a custom value.
+# Simply copy & paste lines you want to change.
+#
+PING_HOST=1.1.1.1
+RUN_EVERY=30
+REENABLE_EVERY=1
+REBOOT_AFTER=10
+
+[ -z $CONF_FILE ] || source $CONF_FILE
+
 c=0
-
-while :; do
-if ping -c 1 1.1.1.1 > /dev/null
-then
-  c=0
-else
-  log -t ProtoWifi "network failure"
-  svc wifi disable
-  sleep 4
-  svc wifi enable
-  c=$((c+1))
-  (( $c > 10 )) && reboot
-fi
-sleep 30
+while true; do
+  if ping -c 1 $PING_HOST > /dev/null; then
+    c=0
+  else
+    c=$((c+1))
+    log -t ProtoWifi "pingreboot: network failure, could not ping $PING_HOST"
+    if (( $c > $REBOOT_AFTER )); then
+       reboot
+    elif (( $c % $REENABLE_EVERY == 0 )); then
+      log -t ProtoWifi "pingreboot: re-enabling WiFi"
+      svc wifi disable
+      sleep 4
+      svc wifi enable
+    fi
+  fi
+  sleep $RUN_EVERY
 done
