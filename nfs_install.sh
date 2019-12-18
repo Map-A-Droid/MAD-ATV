c0d9d4291bc81d09a6b833b9d20eb753d9b55f4e
krzee <4845550+krzee@users.noreply.github.com>
2019-12-18 12:34:35 -0400

    Update nfs_install.sh

--- nfs_install.sh
+++ nfs_install.sh
@@ -1,5 +1,10 @@
-   mkdir -p /cache/recovery
-   touch /cache/recovery/command
-   echo '--update_package=/sdcard/NFS-INJECTOR.zip' >> /cache/recovery/command
-   echo '--wipe_cache' >> /cache/recovery/command
-   reboot recovery
+#!/system/bin/sh
+# Install the NFS module
+# version 1
+
+! [[ -f /sdcard/NFS-INJECTOR.zip ]] && echo "/sdcard/NFS-INJECTOR.zip does not exist" && exit 1
+mkdir -p /cache/recovery
+touch /cache/recovery/command
+echo '--update_package=/sdcard/NFS-INJECTOR.zip' >> /cache/recovery/command
+echo '--wipe_cache' >> /cache/recovery/command
+reboot recovery
