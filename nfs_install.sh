b1fe2a06289fa58e0ca8c03ce07277977c6549f0
ColdShadow80 <46814978+ColdShadow80@users.noreply.github.com>
2020-04-21 00:46:15 +0100

    add download
    * Update nfs_install.sh
    
    * Update nfs_install.sh

--- nfs_install.sh
+++ nfs_install.sh
@@ -1,8 +1,21 @@
 #!/system/bin/sh
 # Install the NFS module
-# version 1.1
+# version 1.2
 #
-! [[ -f /sdcard/NFS-INJECTOR.zip ]] && echo "/sdcard/NFS-INJECTOR.zip does not exist" && exit 1
+NFS_url="https://github.com/Map-A-Droid/MAD-ATV/raw/master/nfs/NFS-INJECTOR-v7.8.FLASHPOINT.zip"
+useragent="Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3"
+
+download(){
+# $1 = url
+# $2 = local path
+# lets see that curl exits successfully
+until /system/bin/curl -s -k -L -A "$useragent" -o "$2" "$1" ;do
+ sleep 60
+done
+}
+
+rm /sdcard/NFS-INJECTOR.zip
+download "$NFS_url" /sdcard/NFS-INJECTOR.zip
 mkdir -p /cache/recovery
 touch /cache/recovery/command
 echo '--update_package=/sdcard/NFS-INJECTOR.zip' >> /cache/recovery/command
