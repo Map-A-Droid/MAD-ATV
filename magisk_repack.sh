5052d1a2012b160625a494b3bde39d647ad9151e
acocalypso <acocalypso@gmail.com>
2020-07-19 22:02:11 +0200

    magisk rehide test

---
+++ magisk_repack.sh
@@ -0,0 +1,14 @@
+#!/system/bin/sh
+echo "repacking magisk"
+input tap 1217 356
+sleep 5
+input tap 1040 74
+sleep 5
+input tap 39 42
+sleep 5
+input tap 150 537
+sleep 5
+input tap 315 552
+sleep 5
+input tap 881 277
+echo "done"
