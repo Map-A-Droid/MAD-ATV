626a78b5b73176e1931bfd99ec85f01c0ab168fa
acocalypso <acocalypso@gmail.com>
2020-07-20 22:14:37 +0200

    reverse default for arch

--- update_mad.sh
+++ update_mad.sh
@@ -154,11 +154,10 @@ reboot=1
 
 update_init(){
 echo "updating init scripts..."
-if [[ "$arch" == "armeabi-v7a" ]]
- /system/bin/curl -o /etc/init.d/16mad -k -s https://raw.githubusercontent.com/Map-A-Droid/MAD-ATV/master/16mad && chmod +x /etc/init.d/16mad
-reboot=1
-else
+if [[ "$arch" == "arm64_v8a" ]]
  /system/bin/curl -o /etc/init.d/42mad -k -s https://raw.githubusercontent.com/Map-A-Droid/MAD-ATV/master/42mad && chmod +x /etc/init.d/42mad
+else
+ /system/bin/curl -o /etc/init.d/16mad -k -s https://raw.githubusercontent.com/Map-A-Droid/MAD-ATV/master/16mad && chmod +x /etc/init.d/16mad
  reboot=1
 }
 
