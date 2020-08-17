6ba6f4a7202f1fa325f3d8b22ecb5750a801a68c
Christian Betz <christian@betz.cc>
2020-08-17 14:07:04 +0200

    pingreboot64 rework

--- pingreboot.sh
+++ pingreboot.sh
@@ -1,35 +1,52 @@
 #!/system/bin/sh
-#version 2.0
+#version 2.1
 
-[ -f /sdcard/pingreboot ] || exit
+lolcat(){
+log -t PingReboot $1
+}
 
-log -t ProtoWifi "pingreboot: started"
+CONF_FILE=/sdcard/pingreboot
+lolcat "checking for $CONF_FILE file"
+if [ -f $CONF_FILE ]; then
+  lolcat "$CONF_FILE found"
+else
+  lolcat "$CONF_FILE not existing, not starting PingReboot"
+  exit 1
+fi
+lolcat "starting"
 
 # These values can be overridden by putting them
 # in $CONF_FILE with a custom value.
 # Simply copy & paste lines you want to change.
 #
-PING_HOST=1.1.1.1
+PING_HOST="google.com"
 RUN_EVERY=30
 REENABLE_EVERY=1
 REBOOT_AFTER=10
 
-source /sdcard/pingreboot
+INTERFACE=$(ip route get 8.8.8.8 | sed -nr 's/.*dev ([^\ ]+).*/\1/p')
+lolcat "identified $INTERFACE as the internet connected interface"
+source $CONF_FILE
 
 c=0
+lolcat "now entering the eternal loop"
 while true; do
   if ping -c 1 "$PING_HOST" > /dev/null; then
+    if (( $c > 0 )); then
+      lolcat "network connection re-established!"
+    fi
     c=0
   else
     c=$((c+1))
-    log -t ProtoWifi "pingreboot: network failure, could not ping $PING_HOST"
+    lolcat "network failure, could not ping $PING_HOST (c=$c)"
     if (( $c > $REBOOT_AFTER )); then
        reboot
     elif (( $c % $REENABLE_EVERY == 0 )); then
-      log -t ProtoWifi "pingreboot: re-enabling WiFi"
-      svc wifi disable
+      lolcat "re-enabling $INTERFACE"
+      ifconfig $INTERFACE down
       sleep 4
-      svc wifi enable
+      ifconfig $INTERFACE up
+      lolcat "interface $INTERFACE re-enabled"
     fi
   fi
   sleep $RUN_EVERY
